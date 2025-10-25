8cf285b8f5c14ed1fe4b5907dd074bcc740cedad
Nguyen Huu Hieu <huuhieu1711@gmail.com>
2025-10-26 01:05:27 +0700

    fix: change to fully package

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.routes import ws_route
+import uvicorn
 
 app = FastAPI(title="Control Server")
 
@@ -10,5 +11,5 @@ def root():
     return {"message": "Control Server is running"}
 
 if __name__ == "__main__":
-    import uvicorn
+
     uvicorn.run("main:app", host="0.0.0.0", port=8000)
